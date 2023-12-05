808e1b3c79d5403b62cc5bc5ad561cf81e1ca42b
wayme <rm.ivaniuk@gmail.com>
2023-12-05 16:38:43 +0200

    add: basename to router

--- src/router.tsx
+++ src/router.tsx
@@ -3,19 +3,24 @@ import HomePage from './pages/HomePage.tsx'
 import ProductDetails from './pages/ProductDetails.tsx'
 import App from './App.tsx'
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+    [
+        {
+            path: '/',
+            element: <App />,
+            children: [
+                {
+                    index: true,
+                    element: <HomePage />,
+                },
+                {
+                    path: 'product/:id',
+                    element: <ProductDetails />,
+                },
+            ],
+        },
+    ],
     {
-        path: '/',
-        element: <App />,
-        children: [
-            {
-                index: true,
-                element: <HomePage />,
-            },
-            {
-                path: 'product/:id',
-                element: <ProductDetails />,
-            },
-        ],
-    },
-])
+        basename: '/react-ts-shop',
+    }
+)
